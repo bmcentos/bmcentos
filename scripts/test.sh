a67c0a044218d97d5a2746f25803446b82ed4368
root <root@localhost.localdomain>
2017-03-14 21:54:22 -0300

    Arquivos adicionados

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Ping em ip em lista
 #Montar um arquivo "urls" com a lista de URLs onion para teste
-list=urls
+list=urls_onion
 Arq=url_onion_status_`date +%d%m%y-%H%M`.log
 echo "#####################################################" >> $Arq
 echo "#####################################################" >> $Arq
@@ -10,10 +10,12 @@ echo "#####################################################" >> $Arq
 echo "`date`" >> $Arq
 cat $list | while read ip
 do
-ping -c 2 $ip > /dev/null
+#ping -c 2 $ip > /dev/null
+proxychains httping -c2 $ip > /dev/null
 if [ $? = 0 ]; then
-echo "ping - Ok $ip" >> $Arq
+echo "ping - Ok => $ip" >> $Arq
 else
-echo "ping - Fail $ip" >> $Arq
+echo "ping - Fail => $ip" >> $Arq
 fi
 done
+exit 0
